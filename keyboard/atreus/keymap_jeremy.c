989ee894ab1278208ea2c1f0693c6e4c102caeee
Jeremy Cowgar <jeremy@cowgar.com>
2016-09-26 16:40:00 -0400

    Layout is now fully functional

--- keyboard/atreus/keymap_jeremy.c
+++ keyboard/atreus/keymap_jeremy.c
@@ -1,5 +1,3 @@
-#include "keymap_common.h"
-
 /*
 This layout was created by Jeremy Cowgar (@jcowgar) who is a programmer by trade
 that suffers from the beginning stages of RSI. As a programmer, letters, symbols
@@ -14,33 +12,49 @@ to time.
 
 The 5 layers can be viewed graphically:
 
-1. Letters - http://www.keyboard-layout-editor.com/#/gists/6861cb9df09ce78efaddf8aa7471e3ac
+1. Letters - http://www.keyboard-layout-editor.com/#/gists/cbed8979bba2df4419c738903a623546
 2. Symbols - http://www.keyboard-layout-editor.com/#/gists/8956a18b508a78e93b9c38ec3fcccaa5
 3. Navigation - http://www.keyboard-layout-editor.com/#/gists/6ed492b714a7f54eb1c5de09b87fd8c4
 4. Numbers - http://www.keyboard-layout-editor.com/#/gists/399ceb5624e8388e48a3a5eacac8e973
 5. Function Keys - http://www.keyboard-layout-editor.com/#/gists/7fd7dc24c7048316f3724b1893c64e89
 */
 
+#include "keymap_common.h"
+
+// Reduce the time to decision of a tap or hold
+#define TAPPING_TERM 150
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
-#define ALPH 0
-#define NUMS 1
-#define FKEY 2
-#define CURS 3
-#define SYMB 4
+#define BOOT 0 // Bootloader (not to be confused with default layer)
+#define ALPH 0 // Alphabet Layer
+#define NUMS 1 // Number Layer
+#define FKEY 2 // Function Key Layer
+#define CURS 3 // Cursor Layer
+#define SYMB 4 // Symbol Layer
+#define OPBS 5 // Option or Backspace
+#define CMDL 6 // Command or Delete
+#define SFSP 7 // Left Shift or Space
+#define SFET 8 // Right Shift or Enter
+
+#define FN_BOOT (KC_FN0 + BOOT)
+#define FN_NUMS (KC_FN0 + NUMS)
+#define FN_FKEY (KC_FN0 + FKEY)
+#define FN_CURS (KC_FN0 + CURS)
+#define FN_SYMB (KC_FN0 + SYMB)
+#define FN_OPBS (KC_FN0 + OPBS)
+#define FN_CMDL (KC_FN0 + CMDL)
+#define FN_SFSP (KC_FN0 + SFSP)
+#define FN_SFET (KC_FN0 + SFET)
 
 // Some handy macros to keep the keymaps clean and easier to maintain
-#define KM_SAVE ACTION_MODS_KEY(MOD_LGUI, KC_D) // Colemak to Qwerty in software playing games
+#define KM_SAVE ACTION_MODS_KEY(MOD_LGUI, KC_D)    // Colemak to Qwerty in software playing games
 #define KM_CLSE ACTION_MODS_KEY(MOD_LGUI, KC_W)
 #define KM_OPEN ACTION_MODS_KEY(MOD_LGUI, KC_SCLN) // Colemak to Qwerty in software playing games
-
 #define KM_COPY ACTION_MODS_KEY(MOD_LGUI, KC_C)
 #define KM_CUT  ACTION_MODS_KEY(MOD_LGUI, KC_X)
 #define KM_PAST ACTION_MODS_KEY(MOD_LGUI, KC_V)
 #define KM_UNDO ACTION_MODS_KEY(MOD_LGUI, KC_Z)
 #define KM_REDO ACTION_MODS_KEY(MOD_LGUI | MOD_LSFT, KC_Z)
-
-#define KM_BSOP ACTION_MODS_TAP_KEY(MOD_LALT, KC_BSPC) // LALT or Backspace
-#define KM_DLCD ACTION_MODS_TAP_KEY(MOD_LGUI, KC_DEL) // LGUI or Delete
 
 // Helpers for my symbol layer
 #define KC_EXLM ACTION_MODS_KEY(MOD_LSFT, KC_1)
@@ -53,26 +67,25 @@ The 5 layers can be viewed graphically:
 #define KC_ASTR ACTION_MODS_KEY(MOD_LSFT, KC_8)
 #define KC_LPRN ACTION_MODS_KEY(MOD_LSFT, KC_9)
 #define KC_RPRN ACTION_MODS_KEY(MOD_LSFT, KC_0)
-#define KC_LABK ACTION_MODS_KEY(MOD_LSFT, KC_COMM)
-#define KC_RABK ACTION_MODS_KEY(MOD_LSFT, KC_DOT)
-#define CM_COLN ACTION_MODS_KEY(MOD_LSFT, KC_P) // Colemak to Qwerty in software playing games
 #define KC_UNDS ACTION_MODS_KEY(MOD_LSFT, KC_MINUS)
+#define KC_PLUS ACTION_MODS_KEY(MOD_LSFT, KC_EQL)
+#define CM_SCLN KC_P                               // Colemak to Qwerty in software playing games
+#define CM_COLN ACTION_MODS_KEY(MOD_LSFT, CM_SCLN) // Colemak to Qwerty in software playing games
 #define KC_QUES ACTION_MODS_KEY(MOD_LSFT, KC_SLSH)
 #define KC_TILD ACTION_MODS_KEY(MOD_LSFT, KC_GRV)
 #define KC_PIPE ACTION_MODS_KEY(MOD_LSFT, KC_BSLS)
 #define KC_LCBR ACTION_MODS_KEY(MOD_LSFT, KC_LABK)
 #define KC_RCBR ACTION_MODS_KEY(MOD_LSFT, KC_RABK)
 #define KC_DQT  ACTION_MODS_KEY(MOD_LSFT, KC_QUOT)
-
-#define KC_PLUS ACTION_MODS_KEY(MOD_LSFT, KC_EQL)
-#define CM_SCLN KC_P // Colemak to Qwerty in software playing games
+#define KC_LABK ACTION_MODS_KEY(MOD_LSFT, KC_COMM)
+#define KC_RABK ACTION_MODS_KEY(MOD_LSFT, KC_DOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [ALPH] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
-  {KC_A,    KC_FN0,  KC_FN1,  KC_F,    KC_G,    KC_TRNS, KC_H,    KC_J,    KC_FN2,  KC_FN3,  KC_SCLN},
-  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KM_BSOP, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LCTL, KC_FN4,  KC_NO,   KC_TAB,  KC_SPC,  KM_DLCD, KC_ENT,  KC_LSFT, KC_NO,   KC_ESC,  KC_RCTL}
+  {KC_A,    FN_NUMS, FN_FKEY, KC_F,    KC_G,    KC_TRNS, KC_H,    KC_J,    FN_CURS, FN_SYMB, KC_SCLN},
+  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    FN_OPBS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
+  {KC_LCTL, FN_BOOT, KC_NO,   KC_TAB,  FN_SFSP, FN_CMDL, FN_SFET, KC_LSFT, KC_NO,   KC_ESC,  KC_RCTL}
 },
 [NUMS] = {
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_ASTR, KC_SLSH, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_SLSH},
@@ -90,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TRNS, KC_BSPC, KC_UP,   KC_DEL,  KC_PGUP, KC_TRNS, KC_TRNS, KM_SAVE, KC_TRNS, KM_OPEN, KC_TRNS},
   {KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KM_UNDO, KC_LALT, KC_TRNS, KC_LGUI, KC_TRNS},
   {KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_MPLY, KM_COPY, KM_REDO, KM_CLSE, KC_TRNS, KC_TRNS, KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_FN4,  KC_TRNS, KC_TAB,  KM_CUT,  KM_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+  {KC_TRNS, KC_TRNS, FN_BOOT, KC_TRNS, KC_TAB,  KM_CUT,  KM_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [SYMB] = {
   {KC_BSLS, KC_EXLM, KC_LABK, KC_RABK, CM_COLN, KC_TRNS, KC_UNDS, KC_DLR,  KC_QUES, KC_TRNS, KC_PERC},
@@ -100,11 +113,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 }};
 
 const uint16_t PROGMEM fn_actions[] = {
-	[0] = ACTION_LAYER_TAP_KEY(NUMS, KC_S),
-	[1] = ACTION_LAYER_TAP_KEY(FKEY, KC_D),
-	[2] = ACTION_LAYER_TAP_KEY(CURS, KC_K),
-	[3] = ACTION_LAYER_TAP_KEY(SYMB, KC_L),
-  [4] = ACTION_FUNCTION(BOOTLOADER)
+  [BOOT] = ACTION_FUNCTION(BOOTLOADER),
+  [NUMS] = ACTION_LAYER_TAP_KEY(NUMS, KC_S),
+  [FKEY] = ACTION_LAYER_TAP_KEY(FKEY, KC_D),
+  [CURS] = ACTION_LAYER_TAP_KEY(CURS, KC_K),
+  [SYMB] = ACTION_LAYER_TAP_KEY(SYMB, KC_L),
+  [OPBS] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_BSPC),
+  [CMDL] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_DEL),
+  [SFSP] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SPC),
+  [SFET] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
